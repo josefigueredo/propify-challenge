a96be701ff6a0867c3f382207ca5995edd6a6bbd
Jose Figueredo <josefigueredo@gmail.com>
2021-02-16 16:41:18 -0300

    Some tests

--- src/test/java/com/propify/challenge/PropertyServiceTest.java
+++ src/test/java/com/propify/challenge/PropertyServiceTest.java
@@ -1,17 +1,27 @@
 package com.propify.challenge;
 
+import com.propify.challenge.model.dto.PropertyReport;
+import com.propify.challenge.model.dto.PropertyType;
 import com.propify.challenge.repository.AddressRepository;
 import com.propify.challenge.repository.PropertyRepository;
 import com.propify.challenge.service.AlertService;
 import com.propify.challenge.service.PropertyService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class PropertyServiceTest {
 
+    @Autowired
     PropertyService propertyService;
 
     @MockBean
@@ -26,7 +36,14 @@ public class PropertyServiceTest {
     // TODO: add at least 3 tests to the method propertyService.propertyReport()
     @Test
     public void testPropertyReport() {
-
+        Mono<PropertyReport> propertyReportMono = propertyService.propertyReport();
+        StepVerifier.create(propertyReportMono)
+                .expectNextMatches(propertyReport ->
+                    propertyReport.getAverageRentPrice() >= 0d &&
+                            propertyReport.getQuantityPerType().containsKey(PropertyType.CONDOMINIUM) &&
+                            propertyReport.getIllinoisQuantity() >= 0
+                )
+                .verifyComplete();
     }
 
 }
